73ab285ce68573ac081d18e6892c27471e537e9b
Shunji Konishi <shun.konishi@nifty.com>
2014-11-10 01:42:51 +0900

    Support post

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,18 +5,21 @@ import play.api.mvc._
 import play.api.libs.iteratee.Enumerator
 
 import com.ning.http.client.AsyncHttpClient
+import com.ning.http.client.Request.EntityWriter
 import com.ning.http.client.Response
 import com.ning.http.client.AsyncCompletionHandler
 import scala.concurrent.{ Future, Promise }
 
 import scala.collection.JavaConverters._
 import scala.concurrent.ExecutionContext.Implicits.global
+import java.io.File
 import java.io.FileInputStream
+import java.io.OutputStream
 
 object Application extends Controller {
 
   val FORCE_HTTPS = true
-  val TARGET_HOST = "excelreport.net"
+  val TARGET_HOST = "excel-report2.herokuapp.com"
 
   def proxy(path: String) = Action.async(parse.raw) { implicit request =>
     def protocol = {
@@ -67,18 +70,36 @@ println("Header: " + kv._1 + ", " + kv._2)
     }
 
     val body = request.body
-    val optStream = if (body.size > 0) Some(new FileInputStream(body.asFile)) else None
-    optStream.foreach { is =>
-      println("file = " + body.asFile)
-      println(java.nio.file.Files.readAllLines(body.asFile.toPath, java.nio.charset.Charset.forName("utf-8")))
-      proxyReq.setBody(is)
+println("BodySize: " + body.size)
+    if (body.size > 0) {
+      val src = body.asFile
+      val dest = File.createTempFile(src.getName(), ".tmp")
+      dest.delete
+      dest.deleteOnExit
+      src.renameTo(dest)
+println("read0: " + dest.length)
+      proxyReq.setBody(new EntityWriter() {
+        override def writeEntity(os: OutputStream): Unit = {
+println("read0: " + dest.length)
+          val is = new FileInputStream(dest)
+          try {
+            val buf = new Array[Byte](8192)
+            var n = is.read(buf)
+            while (n != -1) {
+println("read1: " + new String(buf, 0, n))
+              os.write(buf, 0, n)
+              n = is.read(buf)
+            }
+          } finally {
+            is.close
+            dest.delete
+          }
+        }
+      }, dest.length)
     }
-
-println(optStream)
 
     proxyReq.execute(new AsyncCompletionHandler[Response](){
       override def onCompleted(response: Response): Response = {
-        optStream.foreach(_.close)
         val result = Result(
           ResponseHeader(
             response.getStatusCode,
@@ -92,11 +113,14 @@ println(optStream)
         response
       }
       override def onThrowable(t: Throwable) = {
-        optStream.foreach(_.close)
         ret.failure(t)
       }
     });
     ret.future
+  }
+
+  def test = Action { implicit request =>
+    Ok("tesst")
   }
 
 }
